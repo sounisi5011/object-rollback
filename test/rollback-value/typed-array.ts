6874fd3f88a1bd9d77a95770ea5819b7db27d717
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-12-30 19:36:18 +0900

    ✅ Add tests to rollback Buffer / TypedArray

---
+++ test/rollback-value/typed-array.ts
@@ -0,0 +1,78 @@
+import test from 'ava';
+
+import { ObjectState } from '../../src';
+
+/**
+ * @see https://tc39.es/ecma262/#table-the-typedarray-constructors
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray#TypedArray_objects
+ */
+const typedArrayConstructorList = [
+    Int8Array,
+    Uint8Array,
+    Uint8ClampedArray,
+    Int16Array,
+    Uint16Array,
+    Int32Array,
+    Uint32Array,
+    ...(typeof BigInt64Array === 'function' ? [BigInt64Array] : []), // eslint-disable-line no-undef
+    ...(typeof BigUint64Array === 'function' ? [BigUint64Array] : []), // eslint-disable-line no-undef
+    Float32Array,
+    Float64Array,
+];
+
+type TypedArray = InstanceType<typeof typedArrayConstructorList[number]>;
+
+function setTypedArrayValue(
+    typedArray: TypedArray,
+    index: number,
+    value: number,
+): void {
+    try {
+        typedArray[index] = value;
+    } catch (error) {
+        if (
+            error instanceof TypeError &&
+            /^Cannot convert [0-9]+ to a BigInt$/.test(error.message)
+        ) {
+            typedArray[index] = BigInt(value); // eslint-disable-line no-undef
+            return;
+        }
+        throw error;
+    }
+}
+
+for (const TypedArrayConstructor of typedArrayConstructorList) {
+    test(`should rollback ${TypedArrayConstructor.name} value`, async t => {
+        const value = new TypedArrayConstructor(2);
+        setTypedArrayValue(value, 0, 0xff);
+        const origValueStruct = value.slice();
+
+        const state = new ObjectState();
+        state.set(value);
+
+        setTypedArrayValue(value, 1, 0x07);
+        t.notDeepEqual(value, origValueStruct);
+
+        state.rollback(value);
+        t.deepEqual(value, origValueStruct);
+    });
+
+    test(`should rollback ${TypedArrayConstructor.name} object properties`, t => {
+        const value = new TypedArrayConstructor(2);
+        setTypedArrayValue(value, 0, 0xff);
+        const origValueStruct = value.slice();
+
+        const state = new ObjectState();
+        state.set(value);
+
+        Object.assign(value, {
+            x: 1,
+            y: 2,
+            z: 3,
+        });
+        t.notDeepEqual(value, origValueStruct);
+
+        state.rollback(value);
+        t.deepEqual(value, origValueStruct);
+    });
+}
